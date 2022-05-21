10f5e2918b3e0b8abaa7f276c722d1852c3c957f
Pedro Padilha Portella <pedro.kadjin.sg@gmail.com>
2022-05-21 04:02:33 -0300

    seed de dados

---
+++ API/Data/Seed.cs
@@ -0,0 +1,36 @@
+﻿using API.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace API.Data
+{
+    public class Seed
+    {
+        public static async Task SeedUsers(DataContext context)
+        {
+            if (await context.Users.AnyAsync()) return;
+
+            string userData = await File.ReadAllTextAsync("Data/UserSeedData.json");
+            List<AppUser> users = JsonSerializer.Deserialize<List<AppUser>>(userData);
+
+            foreach (AppUser user in users)
+            {
+                using var hmac = new HMACSHA512();
+
+                user.UserName = user.UserName.ToLower();
+                user.PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes("Pa$$w0rd"));
+                user.PasswordSalt = hmac.Key;
+                
+               Console.WriteLine(user);
+               await context.Users.AddAsync(user);
+               await context.SaveChangesAsync();
+            }
+        }
+    }
+}
---
+++ API/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace API.Extensions
+{
+    public static class DateTimeExtensions
+    {
+        public static int CalculateAge(this DateTime dateBirth)
+        {
+            var today = DateTime.Today;
+            var age = today.Year - dateBirth.Year;
+
+            if (dateBirth.Date > today.AddYears(-age)) 
+            { 
+                age--; 
+            }
+            return age;
+        }
+    }
+}
