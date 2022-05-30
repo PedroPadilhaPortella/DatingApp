6b4a270c5dafd3c3477f5f6b27a8f25bdf15419d
Pedro Padilha Portella <pedro.kadjin.sg@gmail.com>
2022-05-30 03:17:12 -0300

    delete photos

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -106,7 +106,29 @@ namespace API.Controllers
             if (await userRepository.SaveAllAsync()) return NoContent();
 
             return BadRequest("Failed to set Main Photo");
+        }
 
+        [HttpDelete("delete-photo/{photoId}")]
+        public async Task<ActionResult> DeletePhoto(int photoId)
+        {
+            var user = await userRepository.GetUserByUsernameAsync(User.GetUsername());
+            var photo = user.Photos.FirstOrDefault(x => x.Id == photoId);
+
+            if (photo == null) return NotFound();
+
+            if (photo.IsMain) return BadRequest("You cannot delete your main photo");
+
+            if(photo.PublicId != null)
+            {
+                var result = await photoService.DeletePhotoAsync(photo.PublicId);
+                if (result.Error != null) return BadRequest(result.Error.Message);
+            }
+
+            user.Photos.Remove(photo);
+
+            if (await userRepository.SaveAllAsync()) return Ok();
+
+            return BadRequest("Failed to delete the Photo");
         }
     }
 }
